799ab93584ea9b683d2d03b6cde1a291150f7ee4
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2019-11-04 01:16:28 +0300

    IlkCont

---
+++ MVCTest/Controllers/IlkController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTest.Controllers
+{
+    public class IlkController : Controller
+    {
+        // GET: Ilk
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
